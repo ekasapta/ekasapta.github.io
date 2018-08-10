46a29f1ca10627161b8749d811afc108b3676387
Eka Sapta <ekasapta747@gmail.com>
2018-08-10 23:19:15 +0800

    upload first

---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Eka Sapta - My Portfolio - Web Developer</title>
+
+    <link rel="dns-prefetch" href="//cdnjs.cloudflare.com">
+
+    <link rel="icon" sizes="192x192" href="assets/img/favicon.png?_v=20180810">
+    <link rel="stylesheet" href="assets/css/app.css?_v=20180810">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+    <script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "Website",
+        "publisher": {
+            "@type": "Organization",
+            "name": "Eka Sapta",
+            "logo": "https://ekasapta.net/"
+        },
+        "url": "https://ekasapta.net/",
+        "image": "#",
+        "mainEntityOfPage": {
+            "@type": "WebPage",
+            "@id": "https://ekasapta.net"
+        },
+        "description": "Developer advocate of web, part-time blogger &amp; an urban cyclist"
+    }
+    </script>
+</head>
+<body>
+    <nav>
+        <div class="container">
+            <div class="parentNav">
+                <li><a href="#"><i class="fa fa-facebook fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-github fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram fa-lg"></i></a></li>
+            </div>
+        </div>
+    </nav>
+
+    <section class="firstWrap">
+        <img src="assets/img/LeftCircle.svg" alt="Left Circle - ekasapta.net" id="leftCircle">
+        <div class="container">
+            <div class="contentFirstWrap">
+                <div>
+                    <div class="largeFont">
+                        <h1>Hello,</h1>
+                        <h1><span>I'm</span> Eka <span>Sapta</span>.</h1>
+                    </div>
+                    <h4>Back-End Developer.</h4>
+                    <p>Currently focus in Back-End Developer and Web Developer on <a href="https://www.naratamaweb.com"><b>Naratamaweb</b></a>, also love to take a photo using <b>Lumix G7</b> and listen music on <a href="https://open.spotify.com/user/214g7pcydqwppcsluaros5wgi?si=YdUw8nT_QZqluFNNKfRxwA"><b>Spotify</b></a>.</p>
+                </div>
+                <div class="rocketMoon">
+                    <svg id="rocketmoonLayer" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 655.5 668.96"><defs><style>.cls-1,.cls-2,.cls-3{fill:none;stroke:#fff;stroke-linejoin:round;stroke-width:8px;}.cls-1,.cls-3{stroke-linecap:round;}.cls-3{stroke-dasharray:28.02 28.02;}</style></defs><title>Rocket Moon</title><polyline class="cls-1" points="571.8 580.4 532.2 438.79 549.78 388.59 590.69 422.58 634.86 581.96 576.38 598.17"/><line class="cls-1" x1="577.74" y1="489.49" x2="600.72" y2="572.39"/><path class="cls-1" d="M647.86,540.59s31.31-3.14,38.64,43.27c0,0-20.41-13.72-31.1-16.06Z" transform="translate(-35 -38)"/><path class="cls-1" d="M589,556.9s-28.46,13.42-10.85,57c0,0,10.44-22.27,18.39-29.78Z" transform="translate(-35 -38)"/><circle class="cls-1" cx="608.06" cy="511.83" r="15" transform="translate(-149.62 143.02) rotate(-15.49)"/><path class="cls-2" d="M626.52,464.84S593.58,448.37,568,481.05" transform="translate(-35 -38)"/><path class="cls-1" d="M617.3,478.87s-22.85-9.35-42.32,15.95" transform="translate(-35 -38)"/><polyline class="cls-1" points="587.63 595.05 584.2 622.64 641.4 606.79 630.41 594.61"/><path class="cls-1" d="M627.14,669.51l1.43,21.06s-.18,15.27,15.61-5c4.63-6,12,16,12.36,17.33s5.14-14.82,7.72-31.21c.69-4.34,20.37,14.42,20.37,14.42s.81-17.53-7.3-26.35" transform="translate(-35 -38)"/><path class="cls-1" d="M587,522.15" transform="translate(-35 -38)"/><path class="cls-1" d="M590.56,494.88" transform="translate(-35 -38)"/><circle class="cls-2" cx="95" cy="95" r="91"/><circle class="cls-2" cx="51" cy="59" r="7"/><circle class="cls-2" cx="51" cy="141" r="7"/><circle class="cls-2" cx="102" cy="37" r="7"/><circle class="cls-2" cx="161.5" cy="93.5" r="10.5"/><circle class="cls-2" cx="116" cy="83" r="14"/><circle class="cls-2" cx="127.5" cy="140.5" r="17.5"/><circle class="cls-2" cx="145" cy="59" r="5"/><circle class="cls-2" cx="81" cy="123" r="5"/><circle class="cls-2" cx="51" cy="95" r="5"/><path class="cls-1" d="M251,121s5.42.26,14.91,1.55" transform="translate(-35 -38)"/><path class="cls-3" d="M293.48,127.49C364.08,143,503.93,194.75,566,365.43" transform="translate(-35 -38)"/><path class="cls-1" d="M570.56,378.67q2.31,7,4.44,14.33" transform="translate(-35 -38)"/></svg>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="secondWrap">
+        <div class="container">
+            <div class="content">
+                <h4 class="titleSection">Skills</h4>
+                <div>Over 5 years I'm using this languages and other technology for creating website.</div>
+                <div class="webTools">
+                    <div>PHP</div>
+                    <div>GO</div>
+                    <div>JS</div>
+                    <div>MYSQL</div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="thirdWrap">
+        <div class="container">
+            <div class="contentProject">
+                <div class="right">
+                    <div class="titleProject">
+                        <h1>Website Inikos</h1>
+                        <a href="#" class="viewProject">View Project</a>
+
+                        <div class="linkProject">
+                            <li><a href="#"><i class="fa fa-globe fa-lg"></i></a></li>
+                            <li><a href="#"><i class="fa fa-github fa-lg"></i></a></li>
+                            <li><a href="#"><i class="fa fa-comments fa-lg"></i></a></li>
+                        </div>
+                    </div>
+                    <img src="assets/img/projects/coverInikos.jpg" alt="Cover Project Inikos - ekasapta.net" id="coverProject">
+                </div>
+                <div class="left">
+                    <img src="assets/img/projects/coverInikos.jpg" alt="Cover Project Inikos - ekasapta.net" id="coverProject">
+                    <div class="titleProject">
+                        <h1>Naratama PHP</h1>
+                        <a href="https://github.com/ekasapta/naratama" class="viewProject">View Project</a>
+
+                        <div class="linkProject">
+                            <li><a href="https://github.com/ekasapta/naratama"><i class="fa fa-globe fa-lg"></i></a></li>
+                            <li><a href="https://github.com/ekasapta/naratama"><i class="fa fa-github fa-lg"></i></a></li>
+                            <li><a href="https://github.com/ekasapta/naratama"><i class="fa fa-comments fa-lg"></i></a></li>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <a href="#" class="seeAllMyProject">See all my projects</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="fourthWrap" id="contactMe">
+        <div class="container">
+            <div class="content">
+                <h4 class="titleSection">Contact Me</h4>
+                <div class="subSection">
+                    If you want to quick response there's some platform for contact me
+                        <div class="subSectionPlatform">
+                            <li><a href="https://www.facebook.com/messages/t/ekasapta17" data-toggle="tooltip" data-placement="top" title="Contact on Messanger Facebook"><i class="fa fa-facebook-official fa-lg"></i></a></li>
+                            <li><a href="https://www.instagram.com/ekasapta17" data-toggle="tooltip" data-placement="top" title="Contact on DM Instagram"><i class="fa fa-instagram fa-lg"></i></a></li>
+                        </div>
+                    or you can use <a href="#">form contact</a>.
+                </div>
+                <!-- <div class="formSection">
+                    <form action="#" method="post" id="formContact">
+                        <div id="alertForm"></div>
+                        <div class="formGroup">
+                            <label for="fullName"><i class="fa fa-user fa-lg"></i></label>
+                            <input type="text" name="fullname" id="fullname" placeholder="Full Name" class="formControl" autocomplete="off">
+                        </div>
+                        <div class="formGroup">
+                            <label for="email"><i class="fa fa-envelope fa-lg"></i></label>
+                            <input type="text" name="email" id="email" placeholder="Email" class="formControl" autocomplete="off">
+                        </div>
+                        <div class="formGroup">
+                            <label for="subject"><i class="fa fa-comments fa-lg"></i></label>
+                            <input type="text" name="subject" id="subject" placeholder="Subject" class="formControl" autocomplete="off">
+                        </div>
+                        <div class="formGroup">
+                            <textarea name="messages" id="messages" cols="30" rows="10" placeholder="Write your message..." class="formControl" autocomplete="off"></textarea>
+                        </div>
+                        <div class="formGroup">
+                            <button type="submit" class="buttonSend">Submit</button>
+                        </div>
+                    </form>
+                </div> -->
+            </div>
+        </div>
+    </section>
+
+    <section class="fivesWrap">
+        <div class="container">
+            <div class="headContent">
+                My Activity on <a href="https://www.instagram.com/ekasapta17">Instagram</a>
+            </div>
+            <div class="content" id="contentInstagram">
+                <div class="contentPreload">
+                    <div class="boxbeforeFetch"><div class="preloads-img"></div></div>
+                    <div class="boxbeforeFetch"><div class="preloads-img"></div></div>
+                    <div class="boxbeforeFetch"><div class="preloads-img"></div></div>
+                    <div class="boxbeforeFetch"><div class="preloads-img"></div></div>
+                    <div class="boxbeforeFetch"><div class="preloads-img"></div></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="footerWrap">
+        <div class="container">
+            <div class="content">
+                <div>ekasapta &copy; <?php if(date("Y") != "2018") { echo "2018 - ".date("Y"); } else { echo "2018"; } ?></div>
+                <div>
+                    Made in Denpasar, Bali. <br>Hosted on <a href="https://www.domainesia.com/?aff=3407">Domainesia</a> and using CDN Netlify
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="assets/js/main.min.js"></script>
+    <script src="assets/js/instafeed.min.js"></script>
+    <script type="text/javascript">
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
+
+    <script type="text/javascript">
+        var feed = new Instafeed({
+            target: 'contentInstagram',
+            get: 'user',
+            clientId: '25a357e32764438f9c1ea7c29e3e51d4',
+            accessToken: '2068517782.25a357e.51f960f5e2d84fc3a11a5a88b07eef0c',
+            userId: 2068517782,
+            limit: 5,
+            sortBy: 'most-recent',
+            resolution: 'standard_resolution',
+            template: '<a href="{{link}}" data-id="{{id}}" target="_blank"><div class="myInstagramImage"><img src="{{image}}"></div></a>',
+            after: function() {
+                $(".contentPreload").hide();
+            }
+        });
+        feed.run();
+    </script>
+</body>
+</html>
